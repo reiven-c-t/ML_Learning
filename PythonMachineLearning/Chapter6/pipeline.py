82a683a8fc6fa8b52e9a8accf6efeea79c4e2ca5
reiven <seventhtrigger0@gmail.com>
2017-11-26 17:29:18 -0600

    temporary commit

---
+++ PythonMachineLearning/Chapter6/pipeline.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import StratifiedKFold  # not recommend but use it because of the interface
+from sklearn.cross_validation import cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data",
+                 header=None)
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+le.transform(["M", "B"])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# here begin pipeline
+pipe_lr = Pipeline([("scl", StandardScaler()),
+                    ("pca", PCA(n_components=2)),
+                    ("clf", LogisticRegression(random_state=1))
+                    ])
+pipe_lr.fit(X_train, y_train)
+print(pipe_lr.score(X_test, y_test))
+
+# cross validation that everybody love
+kfold = StratifiedKFold(y=y_train, n_folds=10, random_state=1)
+scores = []
+for k, (train, test) in enumerate(kfold):
+    pipe_lr.fit(X_train[train], y_train[train])
+    score = pipe_lr.score(X_train[test], y_train[test])
+    scores.append(score)
+    print('Fold: %s, Class dist.: %s, Acc: %.3f' % (k + 1, np.bincount(y_train[train]), score))
+
+scores = cross_val_score(estimator=pipe_lr,
+                         X=X_train, y=y_train, cv=10, n_jobs=1)
+print("CV accurary scores: %s" % scores)
+
+from sklearn.learning_curve import validation_curve
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+    estimator=pipe_lr,
+    X=X_train,
+    y=y_train,
+    param_name='clf__C',
+    param_range=param_range,
+    cv=10)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+plt.plot(param_range, train_mean,
+         color='blue', marker='o',
+         markersize=5,
+         label='training accuracy')
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+plt.plot(param_range, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+plt.fill_between(param_range,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.show()
+
+# grid search hyper parameter tuning
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+
+pipe_svc = Pipeline([
+    ("scl", StandardScaler()),
+    ("clf", SVC(random_state=1))
+])
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [
+    {
+        "clf__C": param_range,
+        "clf__kernel": ["linear"]
+    },
+    {
+        "clf__C": param_range,
+        "clf__gamma": param_range,
+        "clf__kernel": ["rbf"]
+    }
+]
+
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring="accuracy",
+                  cv=10,
+                  n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
+
+# nested CV?
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring="accuracy",
+                  cv=10,
+                  n_jobs=-1)
+scores = cross_val_score(gs, X, y, scoring="accuracy", cv=5)
+# TODO: 190pから
